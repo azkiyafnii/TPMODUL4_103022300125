434ab6954df6e3acc2197fa459bb1b72e9d8f4e3
azkiyafnii <putriafniazkiya@gmail.com>
2025-03-08 19:10:50 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class KodePos
+{
+    // Dictionary untuk menyimpan daftar kelurahan dan kode posnya
+    private static Dictionary<string, string> daftarKodePos = new Dictionary<string, string>
+    {
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
+    };
+
+    // Method untuk mendapatkan kode pos berdasarkan nama kelurahan
+    public static string GetKodePos(string kelurahan)
+    {
+        // Mengecek apakah kelurahan ada dalam dictionary
+        if (daftarKodePos.ContainsKey(kelurahan))
+        {
+            return daftarKodePos[kelurahan]; // Mengembalikan kode pos jika ditemukan
+        }
+        else
+        {
+            return "Kode pos tidak ditemukan"; // Mengembalikan pesan jika tidak ditemukan
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan nama kelurahan: ");
+        string inputKelurahan = Console.ReadLine(); // Membaca input dari user
+
+        // Memanggil method GetKodePos untuk mencari kode pos
+        string kodePos = KodePos.GetKodePos(inputKelurahan);
+
+        Console.WriteLine($"Kode pos untuk {inputKelurahan}: {kodePos}"); // Menampilkan hasil pencarian
+    }
+}
